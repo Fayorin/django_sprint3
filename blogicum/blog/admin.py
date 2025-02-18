7c24248a45845f96326a0ffd82d2fbb1b638b40e
Fayorin <126774711+Fayorin@users.noreply.github.com>
2025-02-18 16:25:32 +0300

    Add files via upload

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import Category, Location, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'category', 'author')
+    search_fields = ['title']
+    list_filter = ('is_published', 'created_at')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug'
+    )
+    search_fields = ['slug', 'title']
+    list_filter = ('is_published', 'created_at')
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+    list_filter = ('is_published', 'created_at')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
---
+++ blogicum/blog/views.py
@@ -0,0 +1,39 @@
+from django.utils import timezone
+from django.shortcuts import get_object_or_404, render
+
+from .models import Category, Post
+
+POSTS_MAX = 5
+
+
+def posts():
+    return Post.objects.select_related(
+        'category',
+        'location',
+        'author'
+    ).filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=timezone.now()
+    )
+
+
+def index(request):
+    return render(request, 'blog/index.html',
+                  {'post_list': posts()[:POSTS_MAX]})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(posts(), id=id)
+    return render(request, 'blog/detail.html', {'post': post})
+
+
+def category_posts(request, category_slug):
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    context = {'category': category,
+               'post_list': posts().filter(category=category)}
+    return render(request, 'blog/category.html', context)
